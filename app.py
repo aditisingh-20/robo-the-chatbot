a2665c1019dad87cf3f9e2f1596c8c69c7299e48
Aditi Singh <73885110+aditisingh-20@users.noreply.github.com>
2023-03-30 19:48:27 +0530

    Update

--- app.py
+++ app.py
@@ -1,23 +1,13 @@
 from chatbot import chatbot
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request
 from flask_sitemap import Sitemap
 
 app = Flask(__name__)
-ext = Sitemap(app=app)
 app.static_folder = 'static'
 
 @app.route("/")
 def home():
     return render_template("index.html")
-
-@app.route('/')
-def index():
-    pass
-
-@ext.register_generator
-def index():
-    # Not needed if you set SITEMAP_INCLUDE_RULES_WITHOUT_PARAMS=True
-    yield 'index', {}
 
 @app.route("/get")
 def get_bot_response():
